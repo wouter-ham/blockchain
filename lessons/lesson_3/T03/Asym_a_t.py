39429de22131f36e92e1e05fca73502c56c19b60
wouter <wouter@thenextapp.nl>
2022-02-21 17:34:15 +0100

    T03 (not finished yet)

--- lessons/lesson_3/T03/Asym_a_t.py
+++ lessons/lesson_3/T03/Asym_a_t.py
@@ -1,9 +1,20 @@
-
-import Asym
+from Asym import *
 
 # Read the encrypted message from "message_test.enc"
 # Read Keys file from "sample_test.keys"   File format is similar to the previous tutorial
 # Check and print who is the receiver of this message
 
 if __name__ == '__main__':
-    print('This message is for .... ')
+    loaded_keys = load_keys('T03/samples_test.keys')
+
+    with open('T03/message_test.enc', 'rb') as file:
+        content = file.read()
+        for item in loaded_keys:
+            key_name, prv_key, pbc_key = item
+            decrypted = decrypt(b'{content}', prv_key)
+
+            if decrypted:
+                print('sender: ' + key_name)
+                print(decrypted)
+            else:
+                print('wrong')
