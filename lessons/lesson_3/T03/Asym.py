33b46f2b6424944e9b77a92f36306e494a3e4706
wouter <wouter@thenextapp.nl>
2022-02-21 17:09:18 +0100

    copied content to T03 to apply type casting

--- lessons/lesson_3/T03/Asym.py
+++ lessons/lesson_3/T03/Asym.py
@@ -6,65 +6,77 @@ from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import serialization
 import pickle
 
-def generate_keys():
-    private_key = rsa.generate_private_key(public_exponent=65537,key_size=2048)
+from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey, RSAPublicKey
+
+
+def generate_keys() -> (RSAPrivateKey, RSAPublicKey):
+    private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
     public_key = private_key.public_key()
     return private_key, public_key
 
-def encrypt(message, key):
+
+def encrypt(message: bytes, key: RSAPrivateKey | RSAPublicKey) -> bytes | bool:
     try:
-        ciphertext = key.encrypt(message,
-                                padding.OAEP(
-                                    mgf=padding.MGF1(algorithm=hashes.SHA256()),
-                                    algorithm=hashes.SHA256(),
-                                    label=None))
+        ciphertext = key.encrypt(
+            message,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
         return ciphertext
     except:
         return False
 
-def decrypt(ciphertext, key):
+
+def decrypt(ciphertext: bytes, key: RSAPrivateKey | RSAPublicKey) -> bytes | bool:
     try:
         plaintext = key.decrypt(
-                                ciphertext,
-                                padding.OAEP(
-                                    mgf=padding.MGF1(algorithm=hashes.SHA256()),
-                                    algorithm=hashes.SHA256(),
-                                    label=None))
+            ciphertext,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
         return plaintext
     except:
         return False
 
-def save_keys(keys_file_name, keys_list):
-    keys_ser_list = []
+
+def save_keys(keys_file_name: str, keys_list: [(str, RSAPrivateKey, RSAPrivateKey)]) -> None:
+    keys_serialised_list = []
     for item in keys_list:
-        key_name, private_key, public_key = item
-    
-        prv_ser = private_key.private_bytes(
-                    encoding=serialization.Encoding.PEM,
-                    format=serialization.PrivateFormat.TraditionalOpenSSL,
-                    encryption_algorithm=serialization.NoEncryption()  
-                    )
-        pbc_ser = public_key.public_bytes(
-                encoding=serialization.Encoding.PEM,
-                format=serialization.PublicFormat.SubjectPublicKeyInfo
-                )
-        
-        keys_ser_list.append((key_name, prv_ser, pbc_ser))
-    
-    savefile = open(keys_file_name, "wb")
-    pickle.dump(keys_ser_list, savefile)
-    savefile.close()
+        key, private_key, public_key = item
 
-def load_keys(keys_file_name):
-    loadfile = open(keys_file_name, "rb")
-    keys_list_ser = pickle.load(loadfile)
-    loadfile.close()
+        private: RSAPrivateKey = private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.TraditionalOpenSSL,
+            encryption_algorithm=serialization.NoEncryption()
+        )
+        public: RSAPublicKey = public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
 
-    keys_list = []
+        keys_serialised_list.append((key, private, public))
+
+    file = open(keys_file_name, "wb")
+    pickle.dump(keys_serialised_list, file)
+    file.close()
+
+
+def load_keys(keys_file_name: str) -> list:
+    file = open(keys_file_name, "rb")
+    keys_list_ser = pickle.load(file)
+    file.close()
+
+    keys: [(str, RSAPrivateKey, RSAPrivateKey)] = []
     for item in keys_list_ser:
-        key_name, private_key_ser, public_key_ser = item
-        private_key = serialization.load_pem_private_key(private_key_ser,password=None)
-        public_key = serialization.load_pem_public_key(public_key_ser)
+        key, private_key_ser, public_key_ser = item
+        private: RSAPrivateKey = serialization.load_pem_private_key(private_key_ser, password=None)
+        public: RSAPublicKey = serialization.load_pem_public_key(public_key_ser)
 
-        keys_list.append((key_name, private_key, public_key))
-    return keys_list
+        keys.append((key, private, public))
+    return keys
